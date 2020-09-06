86cc8a477578a7f692ab2edb027e62a942215fca
tichise <43707+tichise@users.noreply.github.com>
2020-09-06 15:19:17 +0900

    add

---
+++ Sample/Sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sample
+//
+//  Created by tichise on 2020年9月6日 20/09/06.
+//  Copyright © 2020 tichise. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
