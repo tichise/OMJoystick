b9436ae96988c281ce34b2b3bb6a9e28337d79da
tichise <43707+tichise@users.noreply.github.com>
2021-02-20 16:53:57 +0900

    OMJoystick_Previewsを追加

--- Sources/OMJoyStick.swift
+++ Sources/OMJoyStick.swift
@@ -273,3 +273,21 @@ public struct OMJoystick: View {
         }.padding(40)
     }
 }
+
+struct OMJoystick_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        GeometryReader { geometry in
+            VStack(alignment: .center, spacing: 5) {
+                OMJoystick(isDebug: true, colorSetting: ColorSetting(subRingColor: .red, bigRingNormalBackgroundColor: .green, bigRingDarkBackgroundColor: .blue, bigRingStrokeColor: .yellow, iconColor: .red)) { (joyStickState, stickPosition) in
+                    
+                }.frame(width: geometry.size.width-40, height: geometry.size.width-40)
+                
+                OMJoystick(isDebug: true,  colorSetting: ColorSetting(iconColor: .orange), smallRingRadius: 70, bigRingRadius: 120
+                ) { (joyStickState, stickPosition)  in
+                    
+                }.frame(width: geometry.size.width-40, height: geometry.size.width-40)
+            }
+        }
+    }
+}
