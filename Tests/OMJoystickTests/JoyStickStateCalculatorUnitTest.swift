f42936d92831fa76cacbc3931dc7f048201e8c4c
tichise <43707+tichise@users.noreply.github.com>
2024-01-06 11:16:28 +0900

    Create JoyStickStateCalculatorUnitTest.swift

---
+++ Tests/OMJoystickTests/JoyStickStateCalculatorUnitTest.swift
@@ -0,0 +1,18 @@
+//
+//  JoyStickStateCalculatorUnitTest.swift
+//
+//
+//  Created by tichise on 2024/01/06.
+//
+
+import XCTest
+
+final class JoyStickStateCalculatorUnitTest: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+}
