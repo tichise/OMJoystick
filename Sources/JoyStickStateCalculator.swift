c647b05ae015246db0fe467fd5a0bbb2145ee67d
tichise <43707+tichise@users.noreply.github.com>
2024-01-08 18:33:27 +0900

    Update JoyStickStateCalculator.swift

--- Sources/JoyStickStateCalculator.swift
+++ Sources/JoyStickStateCalculator.swift
@@ -11,10 +11,10 @@ class JoyStickStateCalculator {
     
     /// 八等分ジョイスティックの状態を取得する
     /// - Parameters:
-    /// - stickPosition: ジョイスティックの位置
+    /// - angle: ジョイスティックの角度
     /// - stength: ジョイスティックの強さ
     /// - Returns: JoyStickState
-    static func getJoyStickStateOctant(stickPosition: CGPoint, stength: Int) -> JoyStickState {
+    static func getJoyStickStateOctant(angle: CGFloat, stength: Int) -> JoyStickState {
         var state: JoyStickState = .center
                 
         return state
@@ -41,18 +41,21 @@ class JoyStickStateCalculator {
         return state
     }
     
-    // strengthをメソッドにした
+    // 原点からの距離を取得
     public static func getStrength(stickPosition: CGPoint) -> Int {
         let result = sqrt(stickPosition.x * stickPosition.x + stickPosition.y * stickPosition.y)
         return Int(result)
     }
-    
-    // angleをメソッドにした
+
+    // 真上からの角度（0から360度）を取得
     public static func getAngle(stickPosition: CGPoint) -> CGFloat {
         // atan2を使用して角度を計算
         let point = CGPoint(x: stickPosition.x, y: stickPosition.y)
-        
+
+        // ラジアンから角度に変換
         let angleInRadians = atan2(point.x, point.y)
+
+        // 角度をラジアンに変換
         var angleInDegrees = angleInRadians * 180 / .pi
 
         // 角度を0〜360度に変換
